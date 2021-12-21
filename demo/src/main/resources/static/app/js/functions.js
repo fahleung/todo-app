668da57e0e785b37baa7b247434d889afe6c00d9
fahleung <61280996+fahleung@users.noreply.github.com>
2021-12-21 18:03:29 +0100

    working on tabs & tasklists

--- demo/src/main/resources/static/app/js/functions.js
+++ demo/src/main/resources/static/app/js/functions.js
@@ -178,3 +178,22 @@ function showAll() {
         }
     }
 }
+
+function openTasklist(evt, tasklistName) {
+    var i, tabcontent, tablinks;
+
+    console.log("tasklist name " + tasklistName);
+
+    tabcontent = document.getElementsByClassName("tabs__content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tasklistName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
