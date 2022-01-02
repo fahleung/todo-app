9ca87e5b47c7c2d666e1db22e040a7a46d485cb1
fahleung <61280996+fahleung@users.noreply.github.com>
2022-01-02 12:52:49 +0100

    userservice tests

--- demo/src/test/java/com/fahleung/demo/user/UserServiceTest.java
+++ demo/src/test/java/com/fahleung/demo/user/UserServiceTest.java
@@ -1,16 +1,26 @@
 package com.fahleung.demo.user;
 
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -30,37 +40,80 @@ public class UserServiceTest {
 
     @Test
     void testGetUsers() {
-        // when
         underTest.getUsers();
-        // then
         verify(userRepository).findAll();
     }
 
     @Test
-    @Disabled
     void testLoadUserByUsername() throws UsernameNotFoundException {
-        String username = "Hugo";
-        String email = "hugo@gmail.com";
-        String password = "azerty123";
-
-        // given
-        User user = new User(username, password, password,
-                email);
+        String username = "Thomas";
+        User user = new User("Thomas", "azerty123", "azerty123",
+                "thomas@gmail.com");
         userRepository.save(user);
-
-        assertInstanceOf(UserDetails.class, underTest.loadUserByUsername(username));
+        assertThat(userDao.selectUserByUsername(username).isPresent());
     }
 
     @Test
-    void testRegister() {
-        String username = "Hugo";
-        String email = "hugo@gmail.com";
-        String password = "azerty123";
-
-        // given
-        User user = new User(username, password, password,
-                email);
+    void willThrowIfUsernameDoesntExist() {
+        String username = "Thomas";
+        User user = new User("Thomas", "azerty123", "azerty123",
+                "thomas@gmail.com");
         userRepository.save(user);
+        assertThatThrownBy(() -> underTest.loadUserByUsername(username)).isInstanceOf(UsernameNotFoundException.class)
+                .hasMessageContaining((String.format("Username %s not found", username)));
 
+    }
+
+    @Test
+    void testRegisterShouldReturnAnInstanceOfBindingResult() {
+        User user = new User("Thomas", "azerty123", "azerty123",
+                "thomas@gmail.com");
+        Errors errors = new BeanPropertyBindingResult(user, "user");
+        assertThat(underTest.register(user, (BindingResult) errors)).isInstanceOf(BindingResult.class);
+    }
+
+    @Test
+    void testRegisterIfEmailExists() {
+        User user = new User("Thomas", "azerty123", "azerty123",
+                "thomas@gmail.com");
+        Errors errors = new BeanPropertyBindingResult(user, "user");
+        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        assertTrue(underTest.register(user, (BindingResult) errors).getFieldErrorCount("email") == 1);
+    }
+
+    @Test
+    void testRegisterIfUsernameExists() {
+        User user = new User("Thomas", "azerty123", "azerty123",
+                "thomas@gmail.com");
+        Errors errors = new BeanPropertyBindingResult(user, "user");
+        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        assertTrue(underTest.register(user, (BindingResult) errors).getFieldErrorCount("username") == 1);
+    }
+
+    @Test
+    void testRegisterIfWrongPasswords() {
+        User user = new User("Thomas", "azerty123", "ytreza123",
+                "thomas@gmail.com");
+        Errors errors = new BeanPropertyBindingResult(user, "user");
+        assertTrue(underTest.register(user, (BindingResult) errors).getFieldErrorCount("password") == 1);
+    }
+
+    @Test
+    void testRegisterShouldSaveUser() {
+        // given
+        User user = new User("Thomas", "azerty123", "azerty123",
+                "thomas@gmail.com");
+
+        // when
+        Errors errors = new BeanPropertyBindingResult(user, "user");
+        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(Optional.empty());
+        underTest.register(user, (BindingResult) errors);
+
+        // then
+        ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+
+        verify(userRepository).save(userArgumentCaptor.capture());
+        User capturedUser = userArgumentCaptor.getValue();
+        assertEquals(capturedUser.getUsername(), user.getUsername());
     }
 }
